313e218d1921b3d3350dfcc5f26205ec6d6904fe
ricval <rcvaldes@gmail.com>
2026-05-19 16:17:54 -0600

    Añadido archivo sql para añadir tabla de juzgados, permisos al módulo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,10 +3,11 @@
 Todos los cambios notables en este proyecto serán documentados en este archivo.
 El formato se basa en [Keep a Changelog](https://keepachangelog.com/es-ES/1.1.0/).
 
-## [1.1.4] - 2026-05-15
+## [1.2.0] - 2026-05-19
 
 ### ✨ Mejoras
 
+- Creación de un nuevo módulo llamado `exp_juzgados`. El cual contiene los juzgados de los cuales se pueden pedir expedientes. Utilizado en el servicio "Revisión de Expedientes" de la unidad "Archivo".
 - Añadida la columna `instrucciones` a la tabla de `cit_servicios`. Para incluir instrucciones de cómo llenar el campo de `notas` cuando se crea una cita nueva.
 - Añadido formulario de "Asistencia" en módulo de citas. Para validar la asistencia de un cliente a su cita.
 - Mejora del archivo `README.md`, se incluyó instrucciones de migración.
---
+++ sql/create-exp_juzgados.sql
@@ -0,0 +1,37 @@
+-- Creación de la tabla `exp_juzgados` para su nuevo módulo
+-- Además añade el nuevo módulo a la tabla de módulos y permisos.
+-- Fecha: 2026-05-19
+
+-- Creación de la tabla `exp_juzgados` que utiliza el módulo Juzgados
+CREATE TABLE exp_juzgados (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    clave VARCHAR(16) NOT NULL UNIQUE,
+    descripcion_corta VARCHAR(64) NOT NULL,
+    descripcion VARCHAR(256) NOT NULL,
+    creado TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    modificado TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    estatus CHAR(1) NOT NULL DEFAULT 'A'
+);
+
+-- Hacemos que la tabla de `modulos` el campo `id` tipo `uuid` sea autogenerado
+ALTER TABLE modulos 
+ALTER COLUMN id SET DEFAULT gen_random_uuid();
+
+-- Añadir módulo nuevo 'EXP_JUZGADOS' a la tabla de 'modulos'
+INSERT INTO modulos (nombre, nombre_corto, icono, ruta, en_navegacion)
+VALUES ('EXP_JUZGADOS', 'Juzgados en expedientes', 'mdi mdi-gavel', '/exp_juzgados', TRUE);
+
+-- Hacemos que la tabla de `permisos` el campo `id` tipo `uuid` sea autogenerado
+ALTER TABLE permisos 
+ALTER COLUMN id SET DEFAULT gen_random_uuid();
+
+-- Añadir a la tabla de `permisos` que el rol ADMINISTRADOR pueda administrar el nuevo módulo EXP_JUZGADOS
+INSERT INTO permisos (rol_id, modulo_id, nombre, nivel)
+SELECT 
+    r.id AS rol_id, 
+    m.id AS modulo_id, 
+    'ADMINISTRADOR puede ADMINISTRAR en EXP_JUZGADOS' AS nombre, 
+    4 AS nivel
+FROM roles r, modulos m
+WHERE r.nombre = 'ADMINISTRADOR' 
+  AND m.nombre = 'EXP_JUZGADOS';
