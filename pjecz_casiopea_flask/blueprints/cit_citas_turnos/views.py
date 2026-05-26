1efc29ec5d095d70b9bc5889e89b021b685f50eb
ricval <rcvaldes@gmail.com>
2026-05-26 16:56:28 -0600

    Mejoras en procesamiento de código de barras asistencia

--- pjecz_casiopea_flask/blueprints/cit_citas_turnos/views.py
+++ pjecz_casiopea_flask/blueprints/cit_citas_turnos/views.py
@@ -2,7 +2,7 @@
 Cit Citas Turnos, vistas
 """
 
-from datetime import date, timedelta
+from typing import Tuple
 
 from flask import Blueprint, abort, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
@@ -29,15 +29,30 @@ def before_request():
 
 @cit_citas_turnos.route("/cit_citas_turnos", methods=["GET", "POST"])
 @permission_required(MODULO, Permiso.CREAR)
-def crear_turno():
+def captura():
     """Creación del turno en el sistema de turnos"""
 
     if "barcode" in request.form:
         codigo_barras = request.form["barcode"]
         cit_cita = CitCita.query.filter_by(codigo_barras=codigo_barras).first()
-        if cit_cita:
-            return render_template("cit_citas_turnos/captura.jinja2", cit_cita=cit_cita)
-        else:
-            return render_template("cit_citas_turnos/captura.jinja2", error=True)
+        if cit_cita is None:
+            return render_template("cit_citas_turnos/captura.jinja2", error="¡Su código de barras ya no es válido!")
+        # if cit_cita.oficina.turnos_unidad_id is None:
+        #     return render_template("cit_citas_turnos/captura.jinja2", error="Error del sistema: Esta oficina no tiene unidad de turnos asignada")
+        if cit_cita.turno is None:
+            turno_id, turno = _crear_turno(cit_cita.oficina.turnos_unidad_id)
+            cit_cita.turno_id = turno_id
+            cit_cita.turno = turno
+            # cit_cita.save()
+        return render_template("cit_citas_turnos/captura.jinja2", cit_cita=cit_cita)
 
     return render_template("cit_citas_turnos/captura.jinja2")
+
+
+def _crear_turno(unidad_id: int)-> Tuple[int, str]:
+    """
+    Crea un nuevo turno en el sistema de turnos
+    :return El id del turno generado y el número de turno compuesto.
+    """
+
+    return (418, f"OCP-{1}")
